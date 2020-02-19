23e31c27941bfb11471a92ae80c5f38a9554c5a0
ichi_taro3 <taro3.ichi@gmail.com>
2020-02-19 23:20:41 +0900

    get all timetables of Karasuma Line

--- kyotosubway/kyotosubway/spiders/karasumaline.py
+++ kyotosubway/kyotosubway/spiders/karasumaline.py
@@ -10,10 +10,19 @@ class KarasumalineSpider(scrapy.Spider):
     def parse(self, response):
         # 上り
         for a in response.css('td a[title*=国際会館方面]'):
-            yield {'href': a.attrib.get('href')}
+            yield response.follow(a, cb_kwargs={'updown': "上り"},callback=self.parse_table)
         # 下り
         for a in response.css('td a[title*=近鉄奈良方面]'):
-            yield {'href': a.attrib.get('href')}
+            yield response.follow(a, cb_kwargs={'updown': "下り"}, callback=self.parse_table)
 
-    def parse_table(self, response):
-        pass
+    def parse_table(self, response, updown):
+        timetable = {
+            'station': ' '.join(response.css('div.tt-hed-title::text').extract()) + updown + '方面',
+            'times': {}
+        }
+        for line in response.css('table tr.time.wektime'):
+            hour = line.css('td.heijitsu-tt h3::text').get()
+            timetable['times'][hour] = []
+            for minute in line.css('td'):
+                timetable['times'][hour].extend(minute.css('span.disptnwek::text').getall())
+        yield timetable
