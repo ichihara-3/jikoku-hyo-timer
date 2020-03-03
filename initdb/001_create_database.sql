877926870dde1d3712e589c8db43e7dee8252588
ichi_taro3 <taro3.ichi@gmail.com>
2020-03-03 23:18:23 +0900

    add create statement of timtables

--- initdb/001_create_database.sql
+++ initdb/001_create_database.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE transportation ENCODING = UTF8;
 
+CREATE TABLE timetables (
+  id SERIAL,
+  station_id INT,
+  line_id INT,
+  train_schedule_type_id INT,
+  direction_id INT,
+  destination_id INT,
+  departure_time TIMESTAMP,
+  PRIMARY KEY (id),
+  UNIQUE(station_id, line_id, train_schedule_type_id, direction_id, destination_id, departure_time)
+);
+
