a95c59acc18559485526d52e05d3f2505d2c599c
ichi_taro3 <taro3.ichi@gmail.com>
2020-03-08 18:16:47 +0900

    implement pipline

--- kyotosubway/kyotosubway/pipelines.py
+++ kyotosubway/kyotosubway/pipelines.py
@@ -4,8 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
 
 class KyotosubwayPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = open('result.txt', 'a')
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + '\n'
+        self.file.write(line)
         return item
